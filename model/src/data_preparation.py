f746583c73f9a7a87409b07ce49e5a58cc33b652
nammathalle <manerikarshravan@gmail.com>
2024-10-19 18:26:32 +0530

    created script for data preparation and cleaning

---
+++ model/src/data_preparation.py
@@ -0,0 +1,149 @@
+import os
+import cv2
+import yaml
+import argparse
+
+from tqdm import tqdm
+from pprint import pprint
+
+class_count = dict()
+
+def xcycwhToxyxy(xcycwh, img_width, img_height):
+    """
+    Convert YOLO coordinates to COCO coordinates.
+
+    Args:
+    - yolo_coords: Tuple (x_center, y_center, width, height) in YOLO format (normalized)
+    - img_width: Width of the image
+    - img_height: Height of the image
+
+    Returns:
+    - Tuple (x_min, y_min, width, height) in COCO format
+    """
+    x_center, y_center, width, height = xcycwh
+
+    # Convert normalized values to absolute values
+    x_center_abs = x_center * img_width
+    y_center_abs = y_center * img_height
+    width_abs = width * img_width
+    height_abs = height * img_height
+
+    # Calculate x_min and y_min
+    x_min = x_center_abs - (width_abs / 2)
+    y_min = y_center_abs - (height_abs / 2)
+
+    (x_min, y_min, width_abs, height_abs) = int(x_min), int(y_min), int(width_abs), int(height_abs)
+    return (x_min, y_min, width_abs, height_abs)
+
+def process_dataset(class_names, dataset, new_dataset_dir):
+    """
+    This function processes a dataset of images and their corresponding labels,
+    crops the images based on the provided labels, and saves the cropped images
+    in a new directory structure.
+
+    Parameters:
+    - class_names (list): A list of class names corresponding to the labels in the dataset.
+    - dataset (str): The path to the dataset directory.
+    - new_dataset_dir (str): The path to the new directory where the processed dataset will be saved.
+
+    Returns:
+    None
+    """
+    dataset_types = ['train', 'valid', 'test']
+    images = list()
+
+    dataset_name = os.path.basename(dataset_dir).split('_dataset')[0]
+
+    if not dataset_name in class_count:
+        class_count[dataset_name] = dict()
+
+    for class_name in class_names:
+        if not class_name in class_count[dataset_name]:
+            class_count[dataset_name][class_name] = 0
+        os.makedirs(os.path.join(new_dataset_dir, dataset_name, class_name), exist_ok=True)
+
+    for dataset_type in dataset_types:
+        images_path = os.path.join(dataset, dataset_type, 'images')
+        images.extend([os.path.join(images_path, image) for image in os.listdir(images_path)])
+
+    for image_path in tqdm(images, total=len(images), desc=f'Creating dataset: {os.path.basename(dataset)}'):
+        label_path = os.path.join(image_path.split('images')[0], 'labels', f'{os.path.basename(image_path).split(".jpg")[0]}.txt')
+
+        if os.path.exists(image_path) and os.path.exists(label_path):
+            image = cv2.imread(image_path)
+            h, w = image.shape[:2]
+
+            # Load labels
+            with open(label_path, 'r') as file:
+                labels_yolo = file.readlines()
+
+            try:
+                for label_yolo in labels_yolo:
+                    label = label_yolo.strip().split()
+                    x_center, y_center, width, height = map(float, label[1:])
+
+                    # Convert YOLO coordinates to COCO format
+                    x_min, y_min, width, height = xcycwhToxyxy((x_center, y_center, width, height), w, h)
+
+                    # cv2.rectangle(image, (x_min, y_min), (x_min + width, y_min + height), (0, 0, 255), 2)
+                    cropped_image = image[y_min : y_min + height, x_min : x_min + width]
+
+                    class_count[dataset_name][class_names[int(label[0])]] += 1
+                    cropped_image_path = os.path.join(new_dataset_dir, dataset_name, class_names[int(label[0])], f"{class_count[dataset_name][class_names[int(label[0])]]}.jpg")
+
+                    if cropped_image.shape[0] <= 25 or cropped_image.shape[1] <= 25:
+                        continue
+
+                    if class_names[int(label[0])] == 'coca_cola_diet' and class_count[dataset_name][class_names[int(label[0])]] == 5:
+                        print(cropped_image.shape)
+
+                    cv2.imwrite(cropped_image_path, cropped_image)
+            except Exception as e:
+                print(f'Error processing image: {image_path}, error: {e}')
+        else:
+            print(f'Label file not found for: {label_path.split(dataset)[-1]}')
+
+if __name__ == '__main__':
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--original_dataset_dir", default='/home/nammathalle/work/retail_product_recognition_system/model/original_dataset', required=False, help="Path to the configuration file")
+    parser.add_argument("--new_dataset_dir", default='/home/nammathalle/work/retail_product_recognition_system/model/dataset', required=False, help="Path to the configuration file")
+    args = parser.parse_args()
+
+    original_dataset_dir = args.original_dataset_dir
+    new_dataset_dir = args.new_dataset_dir
+    
+    # Check if the original dataset directory exists
+    if not os.path.isdir(original_dataset_dir):
+        print(f"Error: {original_dataset_dir} is not a valid directory.")
+        exit(1)
+
+    datasets = [folder for folder in os.listdir(original_dataset_dir) if os.path.isdir(os.path.join(original_dataset_dir, folder))]
+    
+    for dataset in datasets:
+        
+        config_file = os.path.join(original_dataset_dir, dataset, 'data.yaml')
+        # Check if the config file exists
+        if os.path.isfile(config_file):
+            class_names = list()
+            dataset_dir = list()
+            try:
+                print(f'Found YoloV7 dataset in: {dataset}')
+                # open config file
+                with open(config_file, "r") as file:
+                    config = yaml.safe_load(file)
+                    
+                class_names = config.get('names')
+
+                dataset_dir = os.path.join(original_dataset_dir, dataset)
+            except Exception as e:
+                print(f'Error parsing config file: {config_file}. Error: {str(e)}')
+                print(f'Skipping: {dataset}')
+                continue
+
+            process_dataset(class_names, dataset_dir, new_dataset_dir)
+            
+        else:
+            print(f'No YoloV7 dataset found, skipping: {dataset}')
+
+    # TODO: Using matplotlib plot class count of each class in each category
