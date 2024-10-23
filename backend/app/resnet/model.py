386dd2d65eb47aa30b2636c78fbd95ca70fe012b
nammathalle <manerikarshravan@gmail.com>
2024-10-24 00:21:58 +0530

    added code for product inference

---
+++ backend/app/resnet/model.py
@@ -0,0 +1,294 @@
+from typing import Any, List, Type, Union, Optional
+
+import torch
+from torch import Tensor
+from torch import nn
+
+__all__ = [
+    "ResNet",
+    "resnet18",
+]
+
+class HierarchicalSoftmax(nn.Module):
+    def __init__(self, h0classes, h1classes, nhid=512,):
+        super(HierarchicalSoftmax, self).__init__()
+
+        self.nhid = nhid
+
+        self.h1classes = h1classes
+        self.h0classes = h0classes
+
+        self.layer_top_W = nn.Parameter(torch.FloatTensor(self.nhid, self.h0classes), requires_grad=True)
+        self.layer_top_b = nn.Parameter(torch.FloatTensor(self.h0classes), requires_grad=True)
+
+        self.layer_bottom_W = nn.Parameter(torch.FloatTensor(self.h0classes, self.nhid, self.h1classes), requires_grad=True)
+        self.layer_bottom_b = nn.Parameter(torch.FloatTensor(self.h0classes, self.h1classes), requires_grad=True)
+
+        self.softmax = nn.Softmax(dim=1)
+
+        self.init_weights()
+
+    def init_weights(self):
+
+        initrange = 0.1
+        self.layer_top_W.data.uniform_(-initrange, initrange)
+        self.layer_top_b.data.fill_(0)
+        self.layer_bottom_W.data.uniform_(-initrange, initrange)
+        self.layer_bottom_b.data.fill_(0)
+
+    def forward(self, inputs, labels = None, parent_labels = None):
+
+        batch_size, d = inputs.size()
+
+        if labels is not None and parent_labels is not None:
+            label_position_top = parent_labels
+            label_position_bottom = labels
+
+            layer_top_logits = torch.matmul(inputs, self.layer_top_W) + self.layer_top_b
+            layer_top_probs = self.softmax(layer_top_logits)
+
+            layer_bottom_logits = torch.squeeze(torch.bmm(torch.unsqueeze(inputs, dim=1), self.layer_bottom_W[label_position_top]), dim=1) + self.layer_bottom_b[label_position_top]
+            layer_bottom_probs = self.softmax(layer_bottom_logits)
+
+            target_probs = layer_top_probs[torch.arange(batch_size).long(), label_position_top] * layer_bottom_probs[torch.arange(batch_size).long(), label_position_bottom]
+
+            return target_probs
+
+        else:
+
+            layer_top_logits = torch.matmul(inputs, self.layer_top_W) + self.layer_top_b
+            layer_top_probs = self.softmax(layer_top_logits)
+
+            class_probs = torch.unsqueeze(layer_top_probs[:,0], dim=1) * self.softmax(torch.matmul(inputs, self.layer_bottom_W[0]) + self.layer_bottom_b[0])
+            class_probs = torch.unsqueeze(class_probs, dim=1)
+
+            for i in range(1, self.h0classes):
+                curent_class_prob = torch.unsqueeze(layer_top_probs[:,i], dim=1) * self.softmax(torch.matmul(inputs, self.layer_bottom_W[i]) + self.layer_bottom_b[i])
+                class_probs = torch.cat((class_probs, torch.unsqueeze(curent_class_prob, dim=1)), dim =1)
+        
+            return class_probs
+
+class _BasicBlock(nn.Module):
+    expansion: int = 1
+
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            stride: int,
+            downsample: Optional[nn.Module] = None,
+            groups: int = 1,
+            base_channels: int = 64,
+    ) -> None:
+        super(_BasicBlock, self).__init__()
+        self.stride = stride
+        self.downsample = downsample
+        self.groups = groups
+        self.base_channels = base_channels
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, (3, 3), (stride, stride), (1, 1), bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, (3, 3), (1, 1), (1, 1), bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = torch.add(out, identity)
+        out = self.relu(out)
+
+        return out
+
+class _Bottleneck(nn.Module):
+    expansion: int = 4
+
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            stride: int,
+            downsample: Optional[nn.Module] = None,
+            groups: int = 1,
+            base_channels: int = 64,
+    ) -> None:
+        super(_Bottleneck, self).__init__()
+        self.stride = stride
+        self.downsample = downsample
+        self.groups = groups
+        self.base_channels = base_channels
+
+        channels = int(out_channels * (base_channels / 64.0)) * groups
+
+        self.conv1 = nn.Conv2d(in_channels, channels, (1, 1), (1, 1), (0, 0), bias=False)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.conv2 = nn.Conv2d(channels, channels, (3, 3), (stride, stride), (1, 1), groups=groups, bias=False)
+        self.bn2 = nn.BatchNorm2d(channels)
+        self.conv3 = nn.Conv2d(channels, int(out_channels * self.expansion), (1, 1), (1, 1), (0, 0), bias=False)
+        self.bn3 = nn.BatchNorm2d(int(out_channels * self.expansion))
+        self.relu = nn.ReLU(True)
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = torch.add(out, identity)
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(
+            self,
+            arch_cfg: List[int],
+            block: Type[Union[_BasicBlock, _Bottleneck]],
+            groups: int = 1,
+            channels_per_group: int = 64,
+            num_classes_h0: int = 100,
+            num_classes_h1: int = 100,
+    ) -> None:
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.dilation = 1
+        self.groups = groups
+        self.base_channels = channels_per_group
+
+        self.conv1 = nn.Conv2d(3, self.in_channels, (7, 7), (2, 2), (3, 3), bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
+        self.relu = nn.ReLU(True)
+        self.maxpool = nn.MaxPool2d((3, 3), (2, 2), (1, 1))
+
+        self.layer1 = self._make_layer(arch_cfg[0], block, 64, 1)
+        self.layer2 = self._make_layer(arch_cfg[1], block, 128, 2)
+        self.layer3 = self._make_layer(arch_cfg[2], block, 256, 2)
+        self.layer4 = self._make_layer(arch_cfg[3], block, 512, 2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.num_features = 512 * block.expansion
+        #self.fc = nn.Linear(self.num_features, num_classes)
+        self.hierarchical_softmax = HierarchicalSoftmax(num_classes_h0, num_classes_h1, self.num_features)
+
+        # Initialize neural network weights
+        self._initialize_weights()
+
+    def _make_layer(
+            self,
+            repeat_times: int,
+            block: Type[Union[_BasicBlock, _Bottleneck]],
+            channels: int,
+            stride: int = 1,
+    ) -> nn.Sequential:
+        downsample = None
+
+        if stride != 1 or self.in_channels != channels * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, channels * block.expansion, (1, 1), (stride, stride), (0, 0), bias=False),
+                nn.BatchNorm2d(channels * block.expansion),
+            )
+
+        layers = [
+            block(
+                self.in_channels,
+                channels,
+                stride,
+                downsample,
+                self.groups,
+                self.base_channels
+            )
+        ]
+        self.in_channels = channels * block.expansion
+        for _ in range(1, repeat_times):
+            layers.append(
+                block(
+                    self.in_channels,
+                    channels,
+                    1,
+                    None,
+                    self.groups,
+                    self.base_channels,
+                )
+            )
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x: Tensor, h0label=None, h1label=None) -> Tensor:
+        probs = self._forward_impl(x, h0label, h1label)
+
+        return probs
+
+    # Support torch.script function
+    def _forward_impl(self, x: Tensor, h0label=None, h1label=None) -> Tensor:
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        #out = self.fc(out)
+
+        probs = self.hierarchical_softmax(out, h0label, h1label)
+
+        return probs
+
+    def _initialize_weights(self) -> None:
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                nn.init.kaiming_normal_(module.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(module, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(module.weight, 1)
+                nn.init.constant_(module.bias, 0)
+
+def resnet18(**kwargs: Any) -> ResNet:
+    model = ResNet([2, 2, 2, 2], _BasicBlock, **kwargs)
+
+    return model
+
+def resnet34(**kwargs: Any) -> ResNet:
+    model = ResNet([3, 4, 6, 3], _BasicBlock, **kwargs)
+
+    return model
+
+def resnet50(**kwargs: Any) -> ResNet:
+    model = ResNet([3, 4, 6, 3], _Bottleneck, **kwargs)
+
+    return model
+
+def resnet101(**kwargs: Any) -> ResNet:
+    model = ResNet([3, 4, 23, 3], _Bottleneck, **kwargs)
+
+    return model
+
+def resnet152(**kwargs: Any) -> ResNet:
+    model = ResNet([3, 8, 36, 3], _Bottleneck, **kwargs)
+
+    return model
